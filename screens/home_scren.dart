df41b4b2c7bfc745b2cb8e27ba198bb858f08e1d
MarcoCD <marco.cd@queretaro.tecnm.mx>
2025-11-12 08:03:15 -0600

    siglechildscrollview
    se agrego un carrusel de elementos horizontal

--- screens/home_scren.dart
+++ screens/home_scren.dart
@@ -7,44 +7,72 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Categorias = [
+      'Ropa',
+      'Calzado',
+      'Accesorios',
+      'Tecnologia',
+      'Hogar',
+      'Deportes',
+      'Ropa',
+      'Calzado',
+      'Accesorios',
+      'Tecnologia',
+      'Hogar',
+      'Deportes',
+    ];
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pantalla Home Screen'),
       ),
-      body:  Center(
-        child: Column(
-          children: [
-            Text('Welcome to the Home Screen!'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed
-                (context, 
-                '/detalle', arguments: 'Charger desde home w');
-    /*             Navigator.push(
-                      context,
-                    MaterialPageRoute(
-                      builder: 
-                      (context) => 
-                      DetallesS(nombre :'Charger')), 
-                ); */
-              },
-               child: Text('Ver detalles del coche')
-              
-               ),
-                Text('Welcome to the Home Screen!'),
-            ElevatedButton(
-              onPressed: () {
- Navigator.pushNamed
-                (context, 
-                '/detalles', arguments: 'Porsche desde home w');
-
-              },
-               child: Text('Ver detalles del Porsche')
-              
-               )
-          ],
+    body: SingleChildScrollView(
+child: Column(
+  children: [
+    Padding(padding: EdgeInsets.all(8.0),
+        child: Text('Categorias' ),
+    ),
+    SizedBox(
+      height: 120,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        separatorBuilder: (_,_) => SizedBox(width: 12), 
+        itemCount: Categorias.length,
+        itemBuilder: (context, index)
+        {
+          final categoria = Categorias[index];
+          return Container(
+            width: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.blueAccent,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(2,2),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Text (categoria)
+                )
+                //Text(categoria),
+              ],
+            ),
+          );
+        }
         ),
-      ),
-    );
+        
+    )
+    
+  ],
+)
+    ));
   }
 }
